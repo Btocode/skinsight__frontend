4388e7fe446ff69d561728cab9ef80ba7def6362
arfan-roky <arfanroky.dev@gmail.com>
2024-12-09 09:18:04 +0600

    feat: add account layout

---
+++ src/app/(main)/(account)/layout.tsx
@@ -0,0 +1,22 @@
+import Footer from "@/components/layout/Footer";
+import AccountTabs from "./_components/AccountTabs";
+import Advertisement from "./_components/Advertisement";
+
+const AccountLayout = ({ children }: { children: React.ReactNode }) => {
+  const userName = "Miranda";
+  return (
+    <div className="min-h-svh bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <h1 className="text-4xl leading-[54px] tracking-tight text-center mb-8">
+          Welcome, <span className="font-bold">{userName}!</span>
+        </h1>
+        <AccountTabs />
+        {children}
+        <Advertisement />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default AccountLayout;
