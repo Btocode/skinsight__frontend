f7c71e7eea28b884c8c8bb603500b1bd6627d5ef
arfan-roky <arfanroky.dev@gmail.com>
2024-12-09 09:16:22 +0600

    feat: add my-regimen page design

---
+++ src/app/(main)/(account)/_components/RegimenCard.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+
+const RegimenCard = () => {
+  return (
+    <div className="w-full rounded-xl bg-white p-6 shadow-[0px_5.13px_33.34px_0px_#2C2C2C17] border border-[#EFEFEF]">
+      <div className="flex flex-col lg:flex-row justify-between items-center gap-6">
+        <div className="space-y-2">
+          <h2 className="text-xl font-semibold text-accent">My Skincare</h2>
+          <p className="text-sm text-accent">Saved on November 11, 2024</p>
+          {/* Product Image */}
+          <div className="flex items-center gap-2 mt-2 ">
+            <div className="-ml-4 h-20 w-20 relative">
+              <Image
+                src={"/products/product1.png"}
+                alt={"Product 1"}
+                fill
+                className="h-full w-full object-contain"
+              />
+            </div>
+            <div className="-ml-10 h-20 w-20 relative">
+              <Image
+                src={"/products/product2.png"}
+                alt={"Product 1"}
+                fill
+                className="h-full w-full object-contain"
+              />
+            </div>
+            <div className="-ml-10 h-20 w-20 relative">
+              <Image
+                src={"/products/product3.png"}
+                alt={"Product 1"}
+                fill
+                className="h-full w-full object-contain"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <button className="flex items-center justify-center gap-2 rounded-full bg-violet-100 py-3 px-8 text-violet-600 transition-colors hover:bg-violet-200">
+            <svg width="25" height="24" viewBox="0 0 25 24" fill="none">
+              <path
+                d="M6.5 7.2002V16.6854C6.5 18.0464 6.5 18.7268 6.70412 19.1433C7.08245 19.9151 7.91157 20.3588 8.76367 20.2454C9.2234 20.1842 9.78964 19.8067 10.9221 19.0518L10.9248 19.0499C11.3737 18.7507 11.5981 18.6011 11.833 18.5181C12.2642 18.3656 12.7348 18.3656 13.166 18.5181C13.4013 18.6012 13.6266 18.7515 14.0773 19.0519C15.2098 19.8069 15.7767 20.1841 16.2364 20.2452C17.0885 20.3586 17.9176 19.9151 18.2959 19.1433C18.5 18.7269 18.5 18.0462 18.5 16.6854V7.19691C18.5 6.07899 18.5 5.5192 18.2822 5.0918C18.0905 4.71547 17.7837 4.40973 17.4074 4.21799C16.9796 4 16.4203 4 15.3002 4H9.7002C8.58009 4 8.01962 4 7.5918 4.21799C7.21547 4.40973 6.90973 4.71547 6.71799 5.0918C6.5 5.51962 6.5 6.08009 6.5 7.2002Z"
+                stroke="currentColor"
+                strokeWidth={2}
+              />
+            </svg>
+
+            <span>View Regimen</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegimenCard;
---
+++ src/app/(main)/(account)/my-regimen/page.tsx
@@ -0,0 +1,7 @@
+import RegimenCard from "../_components/RegimenCard";
+
+const MyRegimenPage = () => {
+  return <RegimenCard />;
+};
+
+export default MyRegimenPage;
