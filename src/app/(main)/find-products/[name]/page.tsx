f97e18b04410939b7cf8919c3cf472018ec9ed38
arfan-roky <arfanroky.dev@gmail.com>
2024-12-12 01:05:19 +0600

    feat: add dynamic page for find products filtering

---
+++ src/app/(main)/find-products/[name]/page.tsx
@@ -0,0 +1,27 @@
+import { notFound } from "next/navigation";
+import SelectLayout from "../_components/SelectLayout";
+
+const folderNames = [
+  "gender",
+  "skin-type",
+  "complexion",
+  "skin-concern",
+  "age",
+  "region",
+  "find-perfect-match",
+];
+export function generateStaticParams() {
+  return folderNames.map((name) => ({ name }));
+}
+
+const page = async ({ params }: { params: Promise<{ name: string }> }) => {
+  const name = (await params).name;
+
+  if (!folderNames.includes(name)) {
+    notFound();
+  }
+
+  return <SelectLayout name={name} />;
+};
+
+export default page;
