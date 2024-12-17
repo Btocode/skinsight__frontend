2a8936fb5a06f90de2a68f1cf5bb5e3abdd957ca
arfan-roky <arfanroky.dev@gmail.com>
2024-12-17 22:18:15 +0600

    feat: add accordion, advertisement, tag components

---
+++ src/components/common/Advertisement.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import React from "react";
+
+const Advertisement = () => {
+  return (
+    <div className="flex flex-col lg:flex-row items-center justify-between gap-4 bg-[#F5F7F6] rounded-lg p-4 mt-10">
+      <div className="h-44 w-60 lg:w-80 relative">
+        <Image src={"/ad/insider.png"} alt="insider" fill />
+      </div>
+      <Image src={"/ad/products.png"} alt="insider" width={500} height={180} />
+      <div className="h-44 w-44 lg:w-48 relative">
+        <Image src={"/ad/discount.png"} alt="insider" fill />
+      </div>
+    </div>
+  );
+};
+
+export default Advertisement;
---
+++ src/components/common/Tag.tsx
@@ -0,0 +1,36 @@
+import { cn } from "@/lib/utils";
+
+type TagProps = {
+  className?: string;
+  circleClassName?: string;
+  variant: "matched" | "best_rated" | "most_viewed" | "popular";
+  children: React.ReactNode;
+};
+
+const Tag = ({ variant, children }: TagProps) => {
+  return (
+    <div
+      className={cn(
+        "flex items-center gap-4 bg-[#13DE9B1A] px-4 py-3 rounded-xl bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-60 text-green-500 relative",
+        {
+          "bg-[#13DE9B1A] text-green-500": variant === "matched",
+          "bg-[#8599FE1A] text-purple-500": variant === "most_viewed",
+          "bg-[#EDAFDF33] text-pink-500": variant === "best_rated",
+          "bg-[#80ADE83D] text-[#80E8DE]": variant === "popular",
+        }
+      )}
+    >
+      <span
+        className={cn("w-4 h-4  blur-sm rounded-full", {
+          "bg-green-300": variant === "matched",
+          "bg-purple-300": variant === "most_viewed",
+          "bg-pink-300": variant === "best_rated",
+          "bg-[#80E8DE]": variant === "popular",
+        })}
+      ></span>
+      <span>{children}</span>
+    </div>
+  );
+};
+
+export default Tag;
