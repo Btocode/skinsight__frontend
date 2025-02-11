877227f6668fee842724be55e5ff2bb0a07430f3
Arfan Roky <arfanroky.dev@gmail.com>
2025-02-12 00:53:31 +0600

    refactor: improve layout and styling in ActionPreference and AddProduct components

--- src/app/(main)/find-products/add-preference/page.tsx
+++ src/app/(main)/find-products/add-preference/page.tsx
@@ -5,14 +5,14 @@ import HeadingPrimary from "@/components/common/HeadingPrimary";
 
 const AddPreferencePage = () => {
   return (
-    <section className="max-w-lg mx-auto flex justify-center lg:items-center min-h-[85svh] py-10 relative">
+    <section className="container lg:max-w-lg mx-auto flex justify-center lg:items-center min-h-[85svh] py-10 relative">
       <div className="flex flex-col lg:gap-3">
         <BackButton buttonProps={{ className: "self-start" }} />
-        <div className="space-y-3">
-          <HeadingPrimary className="lg:text-[42px] lg:leading-[49.98px] tracking-[-0.02em] font-semibold">
+        <div className="space-y-3 my-[12px] lg:my-0">
+          <HeadingPrimary className="text-[28px] lg:text-[42px] leading-[33.32px] lg:leading-[49.98px] tracking-[-0.02em] font-semibold">
             Current product preferences
           </HeadingPrimary>
-          <p className="text-xl font-medium leading-[26px] text-accent">
+          <p className="text-[15px] lg:text-xl font-medium leading-[20.7px] lg:leading-[26px] text-accent">
             Get better results by adding at least 5 products you love and hate
             using and rate them
           </p>
