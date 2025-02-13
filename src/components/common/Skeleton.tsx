64e38e7c0dfccc52f3ef9533359f6f5f2a983c80
Arfan Roky <arfanroky.dev@gmail.com>
2025-02-13 23:47:03 +0600

    feat: add animated skeleton loading image to Skeleton component

--- src/components/common/Skeleton.tsx
+++ src/components/common/Skeleton.tsx
@@ -1,13 +1,21 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 const Skeleton = ({ className }: { className?: string }) => {
   return (
     <div
       className={cn(
-        "bg-gray-500 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 rounded-lg animate-pulse",
+        "relative rounded-lg opacity-10 overflow-hidden",
         className
       )}
-    />
+    >
+      <Image
+        src="/skeleton.gif"
+        alt="skeleton gif"
+        fill
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+      />
+    </div>
   );
 };
 
