57e7e56cdbfef081c02a875228150527921c2b18
Afsan Rahmatullah <talktoafsan@gmail.com>
2025-02-22 22:28:23 +0600

    update | updated storage file

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,18 +1,45 @@
+import isClient from "./isClient";
+
 export const getStorageItem = (key: string): string | null => {
-  if (typeof window !== "undefined") {
+  /**
+ * Retrieves an item from localStorage.
+ * @param key - The key of the item to retrieve.
+ * @returns The item as a string, or null if not found or on the server.
+ */
+
+  if (isClient()) {
     return localStorage.getItem(key);
   }
   return null;
 };
 
 export const setStorageItem = (key: string, value: string): void => {
-  if (typeof window !== "undefined") {
+  /**
+   * Sets an item in localStorage.
+   * @param key - The key of the item to set.
+   * @param value - The value to set.
+   */
+  if (isClient()) {
     localStorage.setItem(key, value);
   }
 };
 
 export const removeStorageItem = (key: string): void => {
-  if (typeof window !== "undefined") {
+  /**
+   * Removes an item from localStorage.
+   * @param key - The key of the item to remove.
+   */
+  if (isClient()) {
     localStorage.removeItem(key);
   }
 };
+
+export const clearStorage = (): void => {
+  /**
+   * Clears all items from localStorage.
+   */
+  if (isClient()) {
+    localStorage.clear();
+  }
+};
+
